8576063d91467c1c4a18d8ebbbfc5dc2e5b7f3ff
DESKTOP-2CHP3MC\Kaustubh Chaudhuri <kaustubhc2014@gmail.com>
2018-09-11 00:28:18 -0700

    Working on v1 of Emoji Predictor

--- Emoji Predictor/emo_utils.py
+++ Emoji Predictor/emo_utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 def read_glove_vecs(glove_file):
-    with open(glove_file, 'r') as f:
+    with open(glove_file, 'r', encoding="utf8") as f:
         words = set()
         word_to_vec_map = {}
         for line in f:
@@ -14,6 +14,8 @@ def read_glove_vecs(glove_file):
             curr_word = line[0]
             words.add(curr_word)
             word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+            
+        
         
         i = 1
         words_to_index = {}
